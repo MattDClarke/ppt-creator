5caa501194cfd5e1b02d593a040233f94fa774f1
Matthew Clarke <m_d_clarke@hotmail.com>
2022-07-07 20:23:56 +0200

    chore: simplified Unsplash API route - removed unnecessary types and removed filter

--- pages/api/unsplash/[query]/[page].ts
+++ pages/api/unsplash/[query]/[page].ts
@@ -1,51 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 import { createApi } from 'unsplash-js';
 
-// from demo - https://stackblitz.com/edit/unsplash-js-typescript?file=index.tsx
-type Photo = {
-  id: number;
-  width: number;
-  height: number;
-  urls: {
-    large: string;
-    regular: string;
-    raw: string;
-    small: string;
-    thumb: string;
-  };
-  alt_description: string;
-  links: {
-    html: string;
-    download_location: string;
-  };
-  color: string | null;
-  user: {
-    username: string;
-    name: string;
-  };
-};
-
-type filteredPhoto = {
-  id: number;
-  width: number;
-  height: number;
-  url_thumb: string;
-  url_regular: string;
-  alt_description: string;
-  username: string;
-  profile: string;
-  trigger_download_API: string;
-};
-
-type Data = {
-  message: string;
-  total?: number;
-  filteredResults?: filteredPhoto[];
-};
-
-export default function getPhotos(
+export default async function getPhotos(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
   if (req.method === 'GET') {
     const perPage = 10;
@@ -56,8 +14,7 @@ export default function getPhotos(
     const api = createApi({
       accessKey: accessToken,
     });
-
-    api.search
+    await api.search
       .getPhotos({
         query,
         page,
@@ -70,26 +27,15 @@ export default function getPhotos(
         } else {
           const feed = result.response;
           // extract total and results array from response
-          const { total } = feed;
-          const results = feed.results as Photo[];
-          // filter feed - dnt need all the info.
-          const filteredResults = results.map(pic => ({
-            id: pic.id,
-            width: pic.width,
-            height: pic.height,
-            url_thumb: pic.urls.thumb,
-            url_regular: pic.urls.regular,
-            alt_description: pic.alt_description,
-            username: pic.user.name,
-            profile: pic.links.html,
-            trigger_download_API: pic.links.download_location,
-          }));
-          res.json({ total, filteredResults, message: 'success' });
+          const total = feed.total;
+          const results = feed.results;
+          res.json({ total, results, message: 'success' });
         }
       })
       // if internet connection issue
       .catch(() => {
         res.status(500).json({ message: 'Internet connection error' });
+        return;
       });
     return;
   }

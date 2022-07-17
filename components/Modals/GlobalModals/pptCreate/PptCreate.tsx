9c3974d7c5305b651d757e1c9de3cc068ce943de
Matthew Clarke <m_d_clarke@hotmail.com>
2022-07-17 19:49:55 +0200

    feat: image multi modal - have multiple Modals instead of swapping modal content
    this preserves scroll state for each modal

--- components/Modals/GlobalModals/pptCreate/PptCreate.tsx
+++ components/Modals/GlobalModals/pptCreate/PptCreate.tsx
@@ -37,88 +37,139 @@ export default function PptCreate({
   const [state, dispatch] = useReducer(reducer, initialState);
 
   const numWords = words.length;
+  const totalSteps = numWords + 1;
 
   // STEP CHECKER
-  if (state.step < 0 || state.step > numWords + 1) {
+  if (state.step < 0 || state.step > totalSteps) {
     throw 'Step Threshold Exceeded';
   }
 
   return (
     <>
-      <Modal
-        isOpen={pptCreateModalIsOpen}
-        closeModal={closeModal}
-        style={{ content: { width: 767 } }}
-      >
-        <Modal.Header closeModal={closeModal} style={{ margin: '0 auto' }}>
-          <LoadingBar numWords={numWords} step={state.step} />
-          {state.step < numWords &&
-            'Choose an image for each word - Click an image to select it'}
-          {state.step === numWords && 'Select ppt options'}
-          {state.step === numWords + 1 && 'ppt creation complete'}
-        </Modal.Header>
-        <div>
-          <Modal.Content
-            style={{
-              overflowY: 'auto',
-              maxHeight: '70vh',
-              textAlign: 'center',
-            }}
-          >
-            {words.map((word, i) => (
-              <div
-                key={word.id}
-                className={i !== state.step ? 'hide' : ''}
-                aria-hidden={i !== state.step ? 'true' : 'false'}
-              >
+      {words.map((word, i) => (
+        <Modal
+          key={word.id + i}
+          isOpen={pptCreateModalIsOpen}
+          closeModal={closeModal}
+          style={{
+            content: { width: 767 },
+            overlay: {
+              visibility: i !== state.step ? 'hidden' : 'visible',
+            },
+          }}
+          aria-hidden={i !== state.step ? 'true' : 'false'}
+        >
+          <Modal.Header closeModal={closeModal} style={{ margin: '0 auto' }}>
+            <LoadingBar numWords={numWords} step={state.step} />
+            {state.step < numWords &&
+              `Choose an image for each word ${word?.word} - Click an image to select it`}
+          </Modal.Header>
+          <div>
+            <Modal.Content
+              style={{
+                overflowY: 'auto',
+                maxHeight: '70vh',
+                textAlign: 'center',
+              }}
+            >
+              <>
                 <div style={{ padding: '1.5rem 0.5rem' }}>
                   {`Word ${state.step + 1} of ${numWords}: "${
-                    word.word
+                    word?.word
                   }" for list "${title}"`}
                 </div>
-                <PptCreateImgSearch word={word.word} />
-              </div>
-            ))}
-            <div
-              className={state.step !== numWords ? 'hide' : ''}
-              aria-hidden={state.step !== numWords ? 'true' : 'false'}
+                <PptCreateImgSearch word={word?.word} />
+              </>
+            </Modal.Content>
+          </div>
+          <Modal.Actions
+            cancelText="Previous"
+            cancelClick={() => dispatch({ type: 'Step_Decrease' })}
+            cancelDisabled={state.step === 0}
+            confirmText={state.step === numWords ? 'Create ppt' : 'Next'}
+            confirmClick={() => dispatch({ type: 'Step_Increase' })}
+            confirmDisabled={state.step >= numWords + 1}
+            hideActions={state.step >= numWords + 1}
+          ></Modal.Actions>
+        </Modal>
+      ))}
+
+      {/* ppt Options step */}
+      {state.step === numWords && (
+        <Modal
+          isOpen={pptCreateModalIsOpen}
+          closeModal={closeModal}
+          style={{
+            content: { width: 767 },
+            overlay: {
+              visibility: state.step !== numWords ? 'hidden' : 'visible',
+              // prevent opacity transition animation
+              opacity: 1,
+            },
+          }}
+          aria-hidden={state.step !== numWords ? 'true' : 'false'}
+        >
+          <Modal.Header closeModal={closeModal} style={{ margin: '0 auto' }}>
+            <LoadingBar numWords={numWords} step={state.step} />
+            Select ppt options
+          </Modal.Header>
+          <div>
+            <Modal.Content
+              style={{
+                overflowY: 'auto',
+                maxHeight: '70vh',
+                textAlign: 'center',
+              }}
             >
-              ppt Options form
-            </div>
-            <div
-              className={state.step !== numWords + 1 ? 'hide' : ''}
-              aria-hidden={state.step !== numWords + 1 ? 'true' : 'false'}
+              <div>ppt Options form</div>
+            </Modal.Content>
+          </div>
+          <Modal.Actions
+            cancelText="Previous"
+            cancelClick={() => dispatch({ type: 'Step_Decrease' })}
+            cancelDisabled={state.step === 0}
+            confirmText={state.step === numWords ? 'Create ppt' : 'Next'}
+            // TODO confirm click when state.step === numWords -> action -> create ppt
+            confirmClick={() => dispatch({ type: 'Step_Increase' })}
+            confirmDisabled={state.step >= numWords + 1}
+            hideActions={state.step >= numWords + 1}
+          ></Modal.Actions>
+        </Modal>
+      )}
+
+      {/* ppt creation complete step */}
+      {state.step === numWords + 1 && (
+        <Modal
+          isOpen={pptCreateModalIsOpen}
+          closeModal={closeModal}
+          style={{
+            content: { width: 767 },
+            overlay: {
+              visibility: state.step !== numWords + 1 ? 'hidden' : 'visible',
+              // prevent opacity transition animation
+              opacity: 1,
+            },
+          }}
+          aria-hidden={state.step !== numWords + 1 ? 'true' : 'false'}
+        >
+          <Modal.Header closeModal={closeModal} style={{ margin: '0 auto' }}>
+            <LoadingBar numWords={numWords} step={state.step} />
+            ppt creation complete
+          </Modal.Header>
+          <div>
+            <Modal.Content
+              style={{
+                overflowY: 'auto',
+                maxHeight: '70vh',
+                textAlign: 'center',
+              }}
             >
-              complete message
-            </div>
-          </Modal.Content>
-        </div>
-        <Modal.Actions
-          cancelText="Previous"
-          cancelClick={() => dispatch({ type: 'Step_Decrease' })}
-          cancelDisabled={state.step === 0}
-          confirmText={state.step === numWords ? 'Create ppt' : 'Next'}
-          // TODO confirm click when state.step === numWords -> action -> create ppt
-          confirmClick={() => dispatch({ type: 'Step_Increase' })}
-          confirmDisabled={state.step >= numWords + 1}
-          hideActions={state.step >= numWords + 1}
-        ></Modal.Actions>
-      </Modal>
-      <style jsx>{`
-        /* Visually hide element - but visible for screen reader */
-        /* https://gomakethings.com/hidden-content-for-better-a11y/#hiding-the-link */
-        .hide {
-          border: 0;
-          clip: rect(0 0 0 0);
-          height: 1px;
-          margin: -1px;
-          overflow: hidden;
-          padding: 0;
-          position: absolute;
-          white-space: nowrap;
-          width: 1px;
-        }
-      `}</style>
+              <div>complete message</div>
+            </Modal.Content>
+          </div>
+          <Modal.Actions hideActions={true}></Modal.Actions>
+        </Modal>
+      )}
     </>
   );
 }
